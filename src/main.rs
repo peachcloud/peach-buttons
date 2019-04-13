debf54e6be6982d8e2f4e1cf07463ad3de2623c7
mycognosist <gnomad@cryptolab.net>
2019-04-13 15:52:00 +0100

    Add code comments

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ extern crate sysfs_gpio;
 extern crate jsonrpc_client_core;
 extern crate jsonrpc_client_http;
 
+use jsonrpc_client_http::HttpTransport;
 use std::thread;
 use sysfs_gpio::{Direction, Edge, Pin};
-use jsonrpc_client_http::HttpTransport;
 
-// json-rpc client
+// define json-rpc server methods (to be called by this client)
 jsonrpc_client!(pub struct PeachMenuClient {
     // send button code to peach-menu
     pub fn press(&mut self, button_code: u8) -> RpcRequest<String>;
 });
 
+// initialize json-rpc client with http transport
+// send button code to peach_menu using json-rpc client
 fn interrupt(pin: u64, button_code: u8) -> sysfs_gpio::Result<()> {
     let input = Pin::new(pin);
     let transport = HttpTransport::new().standalone().unwrap();
@@ -23,76 +25,36 @@ fn interrupt(pin: u64, button_code: u8) -> sysfs_gpio::Result<()> {
         input.set_edge(Edge::FallingEdge)?;
         let mut poller = input.get_poller()?;
         loop {
+            // needs debounce logic!
             match poller.poll(1000)? {
-                Some(_value) => {
-                    client.press(button_code).call().unwrap()
-                },
-                None => "none".to_string()
+                Some(_value) => client.press(button_code).call().unwrap(),
+                None => "none".to_string(),
             };
         }
     })
 }
 
-fn main() {
-
-    thread::spawn(move || {
-        // center joystick
-        let button_code : u8 = 0;
-        match interrupt(462, button_code) {
-            Ok(()) => println!("Interrupting Complete!"),
-            Err(err) => println!("Error: {}", err),
-        }
-    });
-    
-    thread::spawn(move || {
-        // left joystick
-        let button_code : u8 = 1;
-        match interrupt(485, button_code) {
-            Ok(()) => println!("Interrupting Complete!"),
-            Err(err) => println!("Error: {}", err),
-        }
-    });
-    
-    thread::spawn(move || {
-        // right joystick
-        let button_code : u8 = 2;
-        match interrupt(481, button_code) {
-            Ok(()) => println!("Interrupting Complete!"),
-            Err(err) => println!("Error: {}", err),
-        }
-    });
-    
-    thread::spawn(move || {
-        // up joystick
-        let button_code : u8 = 3;
-        match interrupt(475, button_code) {
-            Ok(()) => println!("Interrupting Complete!"),
-            Err(err) => println!("Error: {}", err),
-        }
-    });
-    
-    thread::spawn(move || {
-        // down joystick
-        let button_code : u8 = 4;
-        match interrupt(480, button_code) {
-            Ok(()) => println!("Interrupting Complete!"),
-            Err(err) => println!("Error: {}", err),
-        }
-    });
-    
-    thread::spawn(move || {
-        // A button (#5)
-        let button_code : u8 = 5;
-        match interrupt(463, button_code) {
-            Ok(()) => println!("Interrupting Complete!"),
-            Err(err) => println!("Error: {}", err),
-        }
-    });
-
-    // B button (#6)
-    let button_code : u8 = 6;
-    match interrupt(464, button_code) {
+// spawn a thread to create an interrupt on a single pin
+fn spawn_interrupt(pin: u64, button_code: u8) {
+    thread::spawn(move || match interrupt(pin, button_code) {
         Ok(()) => println!("Interrupting Complete!"),
         Err(err) => println!("Error: {}", err),
-    }
+    });
+}
+
+fn main() {
+    // center joystick
+    spawn_interrupt(462, 0);
+    // left joystick
+    spawn_interrupt(485, 1);
+    // right joystick
+    spawn_interrupt(481, 2);
+    // up joystick
+    spawn_interrupt(475, 3);
+    // down joystick
+    spawn_interrupt(480, 4);
+    // A `#5`
+    spawn_interrupt(463, 5);
+    // B `#6`
+    spawn_interrupt(464, 6);
 }
