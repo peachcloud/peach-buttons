4c68d08fe7f95f0e22e2f5f0875a3fccce3b34d8
mycognosist <gnomad@cryptolab.net>
2019-04-14 11:02:26 +0100

    Remove http server, add tcp server and pubsub functionality for button event subscription

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
+extern crate crossbeam_channel;
 extern crate sysfs_gpio;
-#[macro_use]
-extern crate jsonrpc_client_core;
-extern crate jsonrpc_client_http;
 
-use jsonrpc_client_http::HttpTransport;
+use std::sync::Arc;
 use std::thread;
+
+use jsonrpc_core::futures::Future;
+use jsonrpc_core::*;
+use jsonrpc_pubsub::{PubSubHandler, Session, Subscriber, SubscriptionId};
+use jsonrpc_tcp_server::{RequestContext, ServerBuilder};
+
+use crossbeam_channel::unbounded;
+use crossbeam_channel::Sender;
+
 use sysfs_gpio::{Direction, Edge, Pin};
 
-// define json-rpc server methods (to be called by this client)
-jsonrpc_client!(pub struct PeachMenuClient {
-    // send button code to peach-menu
-    pub fn press(&mut self, button_code: u8) -> RpcRequest<String>;
-});
-
-// initialize json-rpc client with http transport
-// send button code to peach_menu using json-rpc client
-fn interrupt(pin: u64, button_code: u8) -> sysfs_gpio::Result<()> {
+// initialize gpio pin and poller
+// send button code to "subscribe_buttons" rpc method for sink notification
+fn interrupt(pin: u64, button_code: u8, s: Sender<u8>) -> sysfs_gpio::Result<()> {
     let input = Pin::new(pin);
-    let transport = HttpTransport::new().standalone().unwrap();
-    let transport_handle = transport.handle("http://127.0.0.1:3031/").unwrap();
-    let mut client = PeachMenuClient::new(transport_handle);
     input.with_exported(|| {
         input.set_direction(Direction::In)?;
         input.set_edge(Edge::FallingEdge)?;
@@ -27,34 +25,113 @@ fn interrupt(pin: u64, button_code: u8) -> sysfs_gpio::Result<()> {
         loop {
             // needs debounce logic!
             match poller.poll(1000)? {
-                Some(_value) => client.press(button_code).call().unwrap(),
-                None => "none".to_string(),
+                Some(_value) => s.send(button_code).unwrap(),
+                None => (), //return //println!("{:}", "".to_string())
             };
         }
     })
 }
 
 // spawn a thread to create an interrupt on a single pin
-fn spawn_interrupt(pin: u64, button_code: u8) {
-    thread::spawn(move || match interrupt(pin, button_code) {
+fn spawn_interrupt(pin: u64, button_code: u8, s: Sender<u8>) {
+    thread::spawn(move || match interrupt(pin, button_code, s) {
         Ok(()) => println!("Interrupting Complete!"),
         Err(err) => println!("Error: {}", err),
     });
 }
 
 fn main() {
+    // create channel for message passing
+    let (s, r) = unbounded();
+    let (s1, r1) = (s.clone(), r.clone());
+
     // center joystick
-    spawn_interrupt(462, 0);
+    spawn_interrupt(462, 0, s1);
+
+    let s1 = s.clone();
+
     // left joystick
-    spawn_interrupt(485, 1);
+    spawn_interrupt(485, 1, s1);
+
+    let s1 = s.clone();
+
     // right joystick
-    spawn_interrupt(481, 2);
+    spawn_interrupt(481, 2, s1);
+
+    let s1 = s.clone();
+
     // up joystick
-    spawn_interrupt(475, 3);
+    spawn_interrupt(475, 3, s1);
+
+    let s1 = s.clone();
+
     // down joystick
-    spawn_interrupt(480, 4);
+    spawn_interrupt(480, 4, s1);
+
+    let s1 = s.clone();
+
     // A `#5`
-    spawn_interrupt(463, 5);
+    spawn_interrupt(463, 5, s1);
+
+    let s1 = s.clone();
+
     // B `#6`
-    spawn_interrupt(464, 6);
+    spawn_interrupt(464, 6, s1);
+
+    let mut io = PubSubHandler::new(MetaIoHandler::default());
+
+    io.add_subscription(
+        "button_press",
+        (
+            "subscribe_buttons",
+            move |params: Params, _, subscriber: Subscriber| {
+                if params != Params::None {
+                    subscriber
+                        .reject(Error {
+                            code: ErrorCode::ParseError,
+                            message: "Invalid parameters. Subscription rejected".into(),
+                            data: None,
+                        })
+                        .unwrap();
+                    return;
+                }
+
+                let r1 = r1.clone();
+                thread::spawn(move || {
+                    let sink = subscriber
+                        .assign_id_async(SubscriptionId::Number(1))
+                        .wait()
+                        .unwrap();
+
+                    loop {
+                        // listen for gpio interrupt event message
+                        let button_code = r1.recv().unwrap();
+                        // emit button_code to subscriber
+                        match sink
+                            .notify(Params::Array(vec![Value::Number(button_code.into())]))
+                            .wait()
+                        {
+                            Ok(_) => {}
+                            Err(_) => {
+                                println!("Subscription has ended, finishing.");
+                                break;
+                            }
+                        }
+                    }
+                });
+            },
+        ),
+        ("remove_buttons", |_id: SubscriptionId, _| {
+            println!("Closing subscription");
+            futures::future::ok(Value::Bool(true))
+        }),
+    );
+
+    let server = ServerBuilder::with_meta_extractor(io, |context: &RequestContext| {
+        Arc::new(Session::new(context.sender.clone()))
+    })
+    .start(&"127.0.0.1:3030".parse().unwrap())
+    .expect("Unable to start RPC server");
+
+    server.wait();
 }
