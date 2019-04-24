2110c2845eb42727df999045be0bd0a73c3a9753
mycognosist <gnomad@cryptolab.net>
2019-04-24 09:36:37 +0100

    Switch from sysfs_gpio (deprecated) to gpio_cdev

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+#![feature(integer_atomics)]
+
 extern crate crossbeam_channel;
-extern crate sysfs_gpio;
+extern crate gpio_cdev;
 
-use std::sync::Arc;
 use std::thread;
-use std::time::{Duration, Instant};
+use std::sync::Arc;
 
-use sysfs_gpio::{Direction, Edge, Pin};
+use gpio_cdev::*;
 
 use jsonrpc_core::futures::Future;
 use jsonrpc_core::*;
@@ -15,40 +16,22 @@ use jsonrpc_ws_server::{RequestContext, ServerBuilder};
 use crossbeam_channel::unbounded;
 use crossbeam_channel::Sender;
 
-// initialize gpio pin and poller
+// initialize gpio pin and listen for line events
 // send button code to "subscribe_buttons" rpc method for sink notification
-fn interrupt(pin: u64, button_code: u8, s: Sender<u8>) -> sysfs_gpio::Result<()> {
-    let input = Pin::new(pin);
-    input.with_exported(|| {
-        input.set_direction(Direction::In)?;
-        input.set_edge(Edge::FallingEdge)?;
-        let mut poller = input.get_poller()?;
-        loop {
-            let mut start = Instant::now();
-            match poller.poll(1000)? {
-                Some(_value) => {
-                    let debounce_ms = Duration::from_millis(200);
-                    let interrupt_time = Instant::now();
-                    if interrupt_time.duration_since(start) > debounce_ms {
-                        s.send(button_code).unwrap();
-                        let start = interrupt_time;
-                    } else {
-                        ()
-                    }
-                }
-                None => (),
-            };
+fn interrupt_handler(pin: u32, button_code: u8, button_name: String, s: Sender<u8>) {
+    thread::spawn(move || {
+        let mut chip = Chip::new("/dev/gpiochip0").unwrap();
+        let input = chip.get_line(pin).unwrap();
+        
+        for _event in input.events(
+            LineRequestFlags::INPUT,
+            EventRequestFlags::FALLING_EDGE,
+            &button_name
+        ).unwrap() {
+            s.send(button_code).unwrap();
         }
-    })
-}
-
-// spawn a thread to create an interrupt on a single pin
-fn spawn_interrupt(pin: u64, button_code: u8, s: Sender<u8>) {
-    thread::spawn(move || match interrupt(pin, button_code, s) {
-        Ok(()) => println!("Interrupting Complete!"),
-        Err(err) => println!("Error: {}", err),
     });
-}
+}        
 
 fn main() {
     // create channel for message passing
@@ -56,37 +39,37 @@ fn main() {
     let (s1, r1) = (s.clone(), r.clone());
 
     // center joystick
-    spawn_interrupt(462, 0, s1);
+    interrupt_handler(4, 0, "center".to_string(), s1);
 
     let s1 = s.clone();
 
     // left joystick
-    spawn_interrupt(485, 1, s1);
+    interrupt_handler(27, 1, "left".to_string(), s1);
 
     let s1 = s.clone();
 
     // right joystick
-    spawn_interrupt(481, 2, s1);
+    interrupt_handler(23, 2, "right".to_string(), s1);
 
     let s1 = s.clone();
 
     // up joystick
-    spawn_interrupt(475, 3, s1);
+    interrupt_handler(17, 3, "up".to_string(), s1);
 
     let s1 = s.clone();
 
     // down joystick
-    spawn_interrupt(480, 4, s1);
+    interrupt_handler(22, 4, "down".to_string(), s1);
 
     let s1 = s.clone();
 
     // A `#5`
-    spawn_interrupt(463, 5, s1);
+    interrupt_handler(5, 5, "#5".to_string(), s1);
 
     let s1 = s.clone();
 
     // B `#6`
-    spawn_interrupt(464, 6, s1);
+    interrupt_handler(6, 6, "#6".to_string(), s1);
 
     let mut io = PubSubHandler::new(MetaIoHandler::default());
 
@@ -115,7 +98,7 @@ fn main() {
 
                     loop {
                         // listen for gpio interrupt event message
-                        let button_code = r1.recv().unwrap();
+                        let button_code: u8 = r1.recv().unwrap();
                         // emit button_code to subscriber
                         match sink
                             .notify(Params::Array(vec![Value::Number(button_code.into())]))
