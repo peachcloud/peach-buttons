f6289185c7f875ce74a2dbc252198d517cc14595
mycognosist <gnomad@cryptolab.net>
2019-07-21 12:00:00 +0100

    Remove comments

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ extern crate env_logger;
 use std::process;
 
 fn main() {
-    // initialize the logger
     env_logger::init();
 
-    // handle errors returned from `run`
     if let Err(e) = peach_buttons::run() {
         error!("Application error: {}", e);
         process::exit(1);
